83668c4abf839d0dc93a4da328df2c2098127b90
Mosudi I. O. <imosudi@gmail.com>
2025-10-07 21:38:03 +0200

    Update Error Management

--- app/__init__.py
+++ app/__init__.py
@@ -68,6 +68,14 @@ def contact():
 
 
 # Error handlers
+@app.errorhandler(401)
+def unauthorized_error(error):
+    return render_template('401.html'), 401
+
+@app.errorhandler(403)
+def forbidden_error(error):
+    return render_template('403.html'), 403
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
